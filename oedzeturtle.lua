9bf1b98c8ca9fe23e3933ccb22dc51958cd072ef
Oetze van den Broek <s1102228@student.hsleiden.nl>
2021-01-02 18:41:25 +0100

    added first commmit

--- oedzeturtle.lua
+++ oedzeturtle.lua
@@ -1,2 +1,31 @@
+os.loadApi('path')
+local settingsFolder = "./oedzeturtle"
+local locationFolder = path.combine(settingsFolder, "location")
+
+--Direction, 0 = NOrth, 1 = East, 2 = Sout, 3= West
+local defaultLocation = {
+    x = 0,
+    y = 0,
+    z = 0,
+    direction = 0 -- North, east, south, west
+}
+
+local location
+
+if not fs.exists(settingsFolder) then
+    fs.makeDir(settingsFolder)
+end
+
+function saveLocation()
+    fs.open(locationFolder, "w")
+    fs.write(textutils.serialize(location))
+    fs.close()
+end
+
+if not fs.exists(path.combine(locationFolder)) then
+    location = defaultLocation
+    saveLocation()
+end
+
 
 
---
+++ path.lua
@@ -0,0 +1,14 @@
+
+
+
+
+function combine (...)
+    local combined = ""
+    for k, v in pairs(arg) do
+        if k > 1 then
+            combined = combined .. "/"
+        end
+        combined = combined + v
+    end
+    return combined
+end
