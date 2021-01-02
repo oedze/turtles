20dcfa1c6ce0deb42f26d537a88ba51299d197ce
Oetze van den Broek <s1102228@student.hsleiden.nl>
2021-01-02 19:04:03 +0100

    updated

--- path.lua
+++ path.lua
@@ -5,7 +5,7 @@
 function combine (...)
     local combined = ""
     for k, v in pairs(arg) do
-        if k > 1 then
+        if tonumber(k) > 1 then
             combined = combined .. "/"
         end
         combined = combined .. v
